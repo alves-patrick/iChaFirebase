f3d644f923c7352cc6b01d7ae46b2af8c3ecbe41
Patrick <patrickalvesarts@gmail.com>
2023-08-13 17:17:39 -0300

    começo teste

---
+++ iChatFirebase/iChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatFirebaseApp.swift
+//  iChatFirebase
+//
+//  Created by Patrick Alves on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
